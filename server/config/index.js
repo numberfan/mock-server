1db2e41f54f1a4aab103bf9686787028a460a9ff
shirychen <chenfan@dayuw.com>
2017-11-30 15:29:33 +0800

    fix:解决冲突

--- server/config/index.js
+++ server/config/index.js
@@ -7,10 +7,6 @@ module.exports = {
   SERVER: '127.0.0.1',
   PORT: 8880,
   RULES: 'route-rewrite.json',
-<<<<<<< HEAD:server/config/index.js
-  DB_FILE: 'server/db/database.json',
-=======
   DB_FILE: path.resolve(process.cwd(), './server/db/database.json'),
->>>>>>> 2ad5eb3b25145922969838042f44fe7a0484f9cd:server/config/index.js
   STATIC: './src'
 }
